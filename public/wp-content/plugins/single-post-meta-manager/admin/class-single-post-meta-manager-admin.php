cdacd9ebd1be75d1e888ce57b2e204df928011dd
Richard Moss <richard@englishtutorslive.com>
2016-08-04 17:54:16 +0100

    adds file structure and core plugin file for PostMetaManager plugin build

---
+++ public/wp-content/plugins/single-post-meta-manager/admin/class-single-post-meta-manager-admin.php
@@ -0,0 +1,21 @@
+<?php
+
+class SinglePostMetaManagerAdmin
+{
+    protected $version;
+
+    public function __construct($version)
+    {
+        $this->version = $version;
+    }
+
+    public function enqueueStyles()
+    {
+
+    }
+
+    public function addMetaBox()
+    {
+      
+    }
+}
